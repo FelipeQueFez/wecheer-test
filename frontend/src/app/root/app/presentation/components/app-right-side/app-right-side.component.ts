a1b44b390a6d060ad7937f552a82fae3326beda7
“FelipeQueFez“ <felipequefez@gmail.com>
2025-01-15 10:53:55 +0000

    chore:how many events have been received

--- frontend/src/app/root/app/presentation/components/app-right-side/app-right-side.component.ts
+++ frontend/src/app/root/app/presentation/components/app-right-side/app-right-side.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EventItemModel } from '../../../data/models/event-item-model';
 
 @Component({
   selector: 'app-right-side',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './app-right-side.component.scss'
 })
 export class AppRightSideComponent {
-
+  @Input() events: EventItemModel[] = [];
 }
